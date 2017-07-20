9bbb34f5e10b7f7c88444d8c5bebb3af0f0ef8c9
Panziera, Luca <luca.panziera@ft.com>
2017-07-20 17:32:22 +0100

    :white_check_mark: Added unit tests

--- content/handler.go
+++ content/handler.go
@@ -4,8 +4,8 @@ import (
 	tIDUtils "github.com/Financial-Times/transactionid-utils-go"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
-	"net/http"
 	"io"
+	"net/http"
 )
 
 type Handler struct {
@@ -24,23 +24,26 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tID := tIDUtils.GetTransactionIDFromRequest(r)
 
 	cAPIReq, err := http.NewRequest("GET", h.endpoint+"/"+uuid+"?apiKey="+h.apyKey, nil)
-	// TODO FIX HEADERS
-
-	//cAPIReq.Header = r.Header
-	//cAPIReq.Header.Add(tIDUtils.TransactionIDHeader, tID)
 
 	if err != nil {
 		log.WithError(err).WithField(tIDUtils.TransactionIDKey, tID).WithField("url", h.endpoint+"/"+uuid).Error("Error in creating the http request")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	for k, v := range r.Header {
+		cAPIReq.Header[k] = v
+	}
+
 	log.WithField(tIDUtils.TransactionIDKey, tID).WithField("url", h.endpoint+"/"+uuid).Info("Calling CAPI")
 	resp, err := h.httpClient.Do(cAPIReq)
-	defer resp.Body.Close()
 	if err != nil {
 		log.WithError(err).WithField(tIDUtils.TransactionIDKey, tID).WithField("url", h.endpoint+"/"+uuid).Error("Error in calling Content API")
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
+	defer resp.Body.Close()
+
+	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 }
---
+++ content/handler_test.go
@@ -0,0 +1,128 @@
+package content
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const testAPIKey = "testAPIKey"
+
+var testHeaders = http.Header{
+	"Pippo":          []string{"pluto"},
+	"Cartman": []string{"Kyle"},
+}
+
+func TestHappyContentAPI(t *testing.T) {
+	cAPIMock := newContentAPIMock(t, http.StatusOK, aContentBody)
+	defer cAPIMock.Close()
+
+	h := NewHandler(cAPIMock.URL, testAPIKey)
+	r := mux.NewRouter()
+	r.Handle("/drafts/content/{uuid}", h)
+
+	req := httptest.NewRequest("GET", "http://api.ft.com/drafts/content/83a201c6-60cd-11e7-91a7-502f7ee26895", nil)
+	req.Header = testHeaders
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	resp := w.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.NoError(t, err)
+	assert.Equal(t, aContentBody, string(body))
+}
+
+func TestContentAPI404(t *testing.T) {
+	cAPIMock := newContentAPIMock(t, http.StatusNotFound, "not found")
+	defer cAPIMock.Close()
+
+	h := NewHandler(cAPIMock.URL, testAPIKey)
+	r := mux.NewRouter()
+	r.Handle("/drafts/content/{uuid}", h)
+
+	req := httptest.NewRequest("GET", "http://api.ft.com/drafts/content/83a201c6-60cd-11e7-91a7-502f7ee26895", nil)
+	req.Header = testHeaders
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	resp := w.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	assert.NoError(t, err)
+	assert.Equal(t, "not found", string(body))
+}
+
+func TestInvalidURL(t *testing.T) {
+	h := NewHandler(":#", testAPIKey)
+	r := mux.NewRouter()
+	r.Handle("/drafts/content/{uuid}", h)
+
+	req := httptest.NewRequest("GET", "http://api.ft.com/drafts/content/83a201c6-60cd-11e7-91a7-502f7ee26895", nil)
+	req.Header = testHeaders
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	resp := w.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	assert.NoError(t, err)
+	assert.Equal(t, "parse :: missing protocol scheme\n", string(body))
+}
+
+func TestConnectionError(t *testing.T) {
+	h := NewHandler("http://an-endpoint-that-does-not-exist.com", testAPIKey)
+	r := mux.NewRouter()
+	r.Handle("/drafts/content/{uuid}", h)
+
+	req := httptest.NewRequest("GET", "http://api.ft.com/drafts/content/83a201c6-60cd-11e7-91a7-502f7ee26895", nil)
+	req.Header = testHeaders
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	resp := w.Result()
+	_, err := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusBadGateway, resp.StatusCode)
+	assert.NoError(t, err)
+}
+
+func newContentAPIMock(t *testing.T, status int, body string) *httptest.Server {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range testHeaders {
+			assert.Equal(t, v, r.Header[k])
+		}
+
+		if values := r.URL.Query(); len(values["apiKey"]) != 1 || values["apiKey"][0] != testAPIKey {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(status)
+		w.Write([]byte(body))
+	}))
+	return ts
+}
+
+const aContentBody = "{" +
+	"\"id\": \"http://www.ft.com/thing/83a201c6-60cd-11e7-91a7-502f7ee26895\"," +
+	"\"type\": \"http://www.ft.com/ontology/content/Graphic\"," +
+	"\"title\": \"Estimated range of the North Korean missile\"," +
+	"\"alternativeTitles\": {}," +
+	"\"alternativeStandfirsts\": {}," +
+	"\"description\": \"\"," +
+	"\"firstPublishedDate\": \"2017-07-04T18:14:00.000Z\"," +
+	"\"publishedDate\": \"2017-07-04T18:14:00.000Z\"," +
+	"\"requestUrl\": \"http://api.ft.com/content/83a201c6-60cd-11e7-91a7-502f7ee26895\"," +
+	"\"binaryUrl\": \"http://com.ft.imagepublish.prod.s3.amazonaws.com/83a201c6-60cd-11e7-91a7-502f7ee26895\"," +
+	"\"pixelWidth\": 600," +
+	"\"pixelHeight\": 546," +
+	"\"alternativeImages\": {}," +
+	"\"canBeDistributed\": \"verify\"," +
+	"\"canBeSyndicated\": \"verify\"" +
+	"}"
