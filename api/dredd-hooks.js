727136964beda1ec576e0d5e5eeca11380514759
Clark, Peter <peter.clark@ft.com>
2018-02-12 15:49:59 +0000

    :wrench: :green_heart: Re-instating dredd testing, adding ersatz.

--- api/dredd-hooks.js
+++
@@ -1,8 +0,0 @@
-var hooks = require('hooks');
-
-hooks.beforeEach(function (transaction) {
-    if (transaction.name.startsWith("Health > /__gtg") || transaction.name.startsWith("Draft Content > /drafts/nativecontent")) {
-        hooks.log("skipping: " + transaction.name);
-        transaction.skip = true;
-    }
-});
